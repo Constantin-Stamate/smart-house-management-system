126c089859c6d5304b7ceec03bf3abad33549e2d
Constantin_Stamate <constantinstamate.r@gmail.com>
2026-02-16 14:28:46 +0200

    add controllers for application endpoints

---
+++ src/main/java/org/application/smarthouse/controller/FloorController.java
@@ -0,0 +1,9 @@
+package org.application.smarthouse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/floor")
+public class FloorController {
+}
